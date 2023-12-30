f61c0c9f9bc1936792bd6db0a11c8330db57bb17
Cooper <67753381+cp-changchien@users.noreply.github.com>
2023-12-31 01:54:39 +0800

    create include/ and organise file

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/cooper/Desktop/TASA/Orbit_Determination/src/Coordinate/coordinate.cpp" "CMakeFiles/main.dir/src/Coordinate/coordinate.cpp.o" "gcc" "CMakeFiles/main.dir/src/Coordinate/coordinate.cpp.o.d"
+  "/Users/cooper/Desktop/TASA/Orbit_Determination/src/Time/time.cpp" "CMakeFiles/main.dir/src/Time/time.cpp.o" "gcc" "CMakeFiles/main.dir/src/Time/time.cpp.o.d"
+  "/Users/cooper/Desktop/TASA/Orbit_Determination/src/accel/accel.cpp" "CMakeFiles/main.dir/src/accel/accel.cpp.o" "gcc" "CMakeFiles/main.dir/src/accel/accel.cpp.o.d"
+  "/Users/cooper/Desktop/TASA/Orbit_Determination/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.o" "gcc" "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "/Users/cooper/Desktop/TASA/Orbit_Determination/src/propagator.cpp" "CMakeFiles/main.dir/src/propagator.cpp.o" "gcc" "CMakeFiles/main.dir/src/propagator.cpp.o.d"
+  "/Users/cooper/Desktop/TASA/Orbit_Determination/src/utils/input.cpp" "CMakeFiles/main.dir/src/utils/input.cpp.o" "gcc" "CMakeFiles/main.dir/src/utils/input.cpp.o.d"
+  "/Users/cooper/Desktop/TASA/Orbit_Determination/src/utils/rk45.cpp" "CMakeFiles/main.dir/src/utils/rk45.cpp.o" "gcc" "CMakeFiles/main.dir/src/utils/rk45.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/Coordinate/coordinate.cpp.o"
+  "CMakeFiles/main.dir/src/Coordinate/coordinate.cpp.o.d"
+  "CMakeFiles/main.dir/src/Time/time.cpp.o"
+  "CMakeFiles/main.dir/src/Time/time.cpp.o.d"
+  "CMakeFiles/main.dir/src/accel/accel.cpp.o"
+  "CMakeFiles/main.dir/src/accel/accel.cpp.o.d"
+  "CMakeFiles/main.dir/src/main.cpp.o"
+  "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "CMakeFiles/main.dir/src/propagator.cpp.o"
+  "CMakeFiles/main.dir/src/propagator.cpp.o.d"
+  "CMakeFiles/main.dir/src/utils/input.cpp.o"
+  "CMakeFiles/main.dir/src/utils/input.cpp.o.d"
+  "CMakeFiles/main.dir/src/utils/rk45.cpp.o"
+  "CMakeFiles/main.dir/src/utils/rk45.cpp.o.d"
+  "bin/main"
+  "bin/main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
